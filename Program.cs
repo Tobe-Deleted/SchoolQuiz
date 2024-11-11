e800be24ad19b4efea7d9f0535268e262dbc1ddf
oso082024 <oso082024@gmail.com>
2024-11-11 09:40:41 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace loginProject;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool exit = false;
+        while (!exit)
+        {
+            Console.Clear();
+            Console.WriteLine("Login project v 0.1");
+            Console.WriteLine("1. Log in");
+            Console.WriteLine("2. Create new user");
+            Console.WriteLine("0. Exit");
+            ConsoleKey LoggedOutChoice = Console.ReadKey().Key;
+            switch (LoggedOutChoice)
+            {
+                case ConsoleKey.D1:
+                    break;
+                case ConsoleKey.D2:
+                    
+                    break;
+                case ConsoleKey.D0:
+                    exit = true;
+                    break;
+            }
+        }
+
+    }
+}
